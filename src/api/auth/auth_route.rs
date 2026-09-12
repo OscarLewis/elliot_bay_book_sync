c4f20e8490c787946a427a906e9944b7acec5d61
oscar <1351014+OscarLewis@users.noreply.github.com>
2026-09-12 14:00:17 -0700

    Remove commented-out code ref

--- src/api/auth/auth_route.rs
+++ src/api/auth/auth_route.rs
@@ -147,35 +147,3 @@ mod tests {
         Ok(())
     }
 }
-
-/*
-@csrf.exempt
-@kobo.route("/v1/auth/device", methods=["POST"])
-@requires_kobo_auth
-def HandleAuthRequest():
-    log.debug('Kobo Auth request')
-    if config.config_kobo_proxy:
-        try:
-            return redirect_or_proxy_request()
-        except Exception:
-            log.error("Failed to receive or parse response from Kobo's auth endpoint. Falling back to un-proxied mode.")
-    return make_calibre_web_auth_response()
-    def make_calibre_web_auth_response():
-
-# As described in kobo_auth.py, CalibreWeb doesn't make use practical use of this auth/device API call for
-# authentation (nor for authorization). We return a dummy response just to keep the device happy.
-content = request.get_json()
-AccessToken = base64.b64encode(os.urandom(24)).decode('utf-8')
-RefreshToken = base64.b64encode(os.urandom(24)).decode('utf-8')
-return make_response(
-    jsonify(
-        {
-            "AccessToken": AccessToken,
-            "RefreshToken": RefreshToken,
-            "TokenType": "Bearer",
-            "TrackingId": str(uuid.uuid4()),
-            "UserKey": content.get('UserKey', ""),
-        }
-    )
-)
-*/
